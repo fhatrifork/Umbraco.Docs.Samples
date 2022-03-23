2921e9335f80fc91f227d1b0e4b6bd4efc4341e1
Frederik Højholt Andersen <61012826+fhatrifork@users.noreply.github.com>
2022-03-23 08:52:54 +0100

    Remove whitespace

--- Umbraco.Docs.Samples.Web/RecurringHostedService/UmbracoBuilderHostedServiceExtensions.cs
+++ Umbraco.Docs.Samples.Web/RecurringHostedService/UmbracoBuilderHostedServiceExtensions.cs
@@ -4,11 +4,11 @@ using Umbraco.Cms.Core.DependencyInjection;
 namespace Umbraco.Docs.Samples.Web.RecurringHostedService
 {
     public static class UmbracoBuilderHostedServiceExtensions
-    {       
+    {
         public static IUmbracoBuilder AddCustomHostedServices(this IUmbracoBuilder builder)
         {
             builder.Services.AddHostedService<CleanUpYourRoom>();
             return builder;
-        }        
+        }
     }
 }
